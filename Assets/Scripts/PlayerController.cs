8f19ebe0758a628d1fac32cd3465061e9377259b
Dacian-Lupei <94475016+Dacian-Lupei@users.noreply.github.com>
2024-11-23 18:24:23 +0200

    Part 12 tutorial done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
+    public float jumpImpulse = 10f;
+    public float airWalkSpeed = 3f;
     Vector2 moveInput;
+    TouchingDirections touchingDirections;
 
     Rigidbody2D rb;
     Animator animator;
@@ -18,19 +21,43 @@ public class PlayerController : MonoBehaviour
     {
         get
         {
-            if (IsMoving)
-                return walkSpeed;
+            if (CanMove)
+            {
+                if (IsMoving && !touchingDirections.IsOnWall)
+                {
+                    if (touchingDirections.IsGrounded)
+                        return walkSpeed;
+                    else
+                        return airWalkSpeed;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
             else
+            {
                 return 0;
+            }
         }
     }
 
+
+    public bool CanMove
+    {
+        get
+        {
+            return animator.GetBool(AnimationStrings.canMove);
+        }
+    }
     [SerializeField]
     private bool _isMoving = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
     public bool IsMoving
     {
@@ -41,12 +68,12 @@ public class PlayerController : MonoBehaviour
         private set
         {
             _isMoving = value;
-            animator.SetBool("isMoving", value);
+            animator.SetBool(AnimationStrings.isMoving, value);
         }
     }
 
     public bool _isFacingRight = true;
-   
+
     public bool IsFacingRight { 
         get 
         { 
@@ -60,34 +87,6 @@ public class PlayerController : MonoBehaviour
             }
             _isFacingRight = value;
         }
-    }
-
-    //[SerializeField]
-    //private bool _isRunning = false;
-
-    //public bool IsRunning
-    //{
-    //    get
-    //    {
-    //        return _isRunning;
-    //    }
-    //    set
-    //    {
-    //        _isRunning = value;
-    //        animator.SetBool("isRunning", value);
-    //    }
-    //}
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     private void FixedUpdate()
@@ -116,15 +115,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    //public void OnRun(InputAction.CallbackContext context)
-    //{
-    //    if (context.started)
-    //    {
-    //        IsRunning = true;
-    //    }
-    //    else if (context.canceled)
-    //    {
-    //        IsRunning = false;
-    //    }
-    //}
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //TODO Check if alive as well
+        if (context.started && touchingDirections.IsGrounded && CanMove)
+        {
+            animator.SetTrigger(AnimationStrings.jumpTrigger);
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            animator.SetTrigger(AnimationStrings.attackTrigger);
+        }
+    }
+
 }
