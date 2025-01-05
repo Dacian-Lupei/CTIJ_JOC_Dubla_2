f4b18338f5c029b8d95f1e61595c5ffed4aa027b
Dacian-Lupei <94475016+Dacian-Lupei@users.noreply.github.com>
2025-01-05 13:38:57 +0200

    VoidGata+BackgroundLVL1

---
+++ Assets/Scripts/Void.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Void : MonoBehaviour
+{
+    public GameObject player;
+    public Transform RespawnPoint;
+    public GameObject enemy;
+    public int SceneReset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //player.transform.position = RespawnPoint.position;
+            SceneManager.LoadScene(SceneReset);
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(enemy);
+        }
+    }
+}
